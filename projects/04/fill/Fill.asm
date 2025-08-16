721fabf38034dad8548a27b146c633e88cba06a3
hayaten0415 <hayaten415@gmail.com>
2025-08-16 21:17:09 +0900

    solve 04 Mult but don't solve Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,61 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// 512px * 256px / 16 = 8192
+
+(LOOP)
+    @KBD
+    D=M              // D = キー入力の状態
+    @BLACK
+    D;JNE            // D ≠ 0 → BLACK へジャンプ
+
+(WHITE)
+    @0
+    D=A              // D = 0（白）
+    @COLOR
+    M=D
+    @DRAW
+    0;JMP
+
+(BLACK)
+    @0
+    D=A
+    D=!D             // D = -1（0xFFFF）黒
+    @COLOR
+    M=D
+
+(DRAW)
+    @SCREEN
+    D=A
+    @addr
+    M=D              // addr = SCREEN 開始位置
+
+    @8192
+    D=A
+    @end
+    M=D              // end = 8192 ワード
+
+(CLEAR_LOOP)
+    @addr
+    A=M
+    D=M              // 現在の内容（無視）
+
+    @COLOR
+    D=M
+    @addr
+    A=M
+    M=D              // 塗りつぶし
+
+    @addr
+    M=M+1            // 次のワードへ
+
+    @end
+    D=M
+    @addr
+    D=M-D
+    @LOOP
+    D;JEQ            // 全て塗り終わったらループへ戻る
+
+    @CLEAR_LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,10 +1,35 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
+// File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @R2
+    M=0 // R2=0
+    @i
+    M=0 // i=0
+(LOOP)
+    // if(i == R1) goto END
+    @i
+    D=M
+    @R1
+    D=D-M // D=i-R1
+    @END
+    D;JEQ
+    // D=R0
+    @R0
+    D=M
+    // R2=R0+R2
+    @R2
+    M=D+M
+    // i = i + 1
+    @i
+    M=M+1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
